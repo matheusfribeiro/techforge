997a0c29e773057960365ff2c9547812bde9a026
matheusfribeiro <matheusfribeiro2112@gmail.com>
2025-07-01 11:35:15 -0300

    feat: setting up database with env vars

--- src/main/java/com/techforge/persistance/Database.java
+++ src/main/java/com/techforge/persistance/Database.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/tech_forge";
-    private static final String USER = "root";
-    private static final String PASSWORD = "n3admin";
-
-
+    private static final String DEFAULT_URL = "jdbc:mysql://localhost:3306/tech_forge";
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASSWORD = "n3admin";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String url = System.getProperty("db.url",  System.getenv().getOrDefault("DB_URL", DEFAULT_URL));
+        String user = System.getProperty("db.user", System.getenv().getOrDefault("DB_USER", DEFAULT_USER));
+        String password = System.getProperty("db.password", System.getenv().getOrDefault("DB_PASS", DEFAULT_PASSWORD));
+        return DriverManager.getConnection(url, user, password);
     }
 }
